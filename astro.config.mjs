99813c7877567d617c29da202f9f3d5b5d72a292
Alkaid114514 <xinlai.gao2006@outlook.com>
2025-09-06 13:35:01 +0800

    修改站点地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import fuwariLinkCard from "./src/plugins/fuwari-link-card.ts";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://a1kari8.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
